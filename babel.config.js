1f6b9f581d3ee7b44c1e2b5519f14d7eda1d4570
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2022-04-16 15:14:48 -0300

    chore: configure babel and update tsconfig file
    Configure babel to to use alias to the import paths.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          assets: './src/assets',
+          components: './src/components',
+          navigation: './src/navigation',
+          routes: './src/routes',
+          state: './src/state',
+          styles: './src/styles',
+          utils: './src/utils',
+        },
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        root: './src',
+      },
+    ],
+    'dynamic-import-node',
+    'graphql-tag',
+    'import-graphql',
+  ],
   presets: ['module:metro-react-native-babel-preset'],
 };

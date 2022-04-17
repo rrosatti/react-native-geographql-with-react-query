990076d9d117ffe9c0c229baa1fee2e80eb664f8
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2022-04-17 12:54:40 -0300

    feat(state/create-client): add helper to create an instance of ApolloClient
    To keep it simple, we're using a basic configuration for the ApolloClient:
    
    * using 'InMemoryCache' as our cache
    * a 'HttpLink' using the server url: https://api.geographql.renzooo.com/graphql
    * 'onError' to capture and log GraphQl errors

---
+++ src/state/create-client.ts
@@ -0,0 +1,66 @@
+import { ApolloClient, ApolloLink, HttpLink } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import type { NormalizedCacheObject } from '@apollo/client/cache';
+import { InMemoryCache } from '@apollo/client/cache';
+
+export type ClientDestroyer = () => Promise<void>;
+
+export type Client = ApolloClient<NormalizedCacheObject>;
+
+const httpLink = new HttpLink({
+  // ideally it would be in an .env file
+  uri: 'https://api.geographql.renzooo.com/graphql',
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      // eslint-disable-next-line no-console
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  // eslint-disable-next-line no-console
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const cache = new InMemoryCache();
+
+const init = async (
+  client: ApolloClient<NormalizedCacheObject>,
+): Promise<() => Promise<void>> => {
+  const destroyClient = async (): Promise<void> => {
+    await client.stop();
+  };
+  return destroyClient;
+};
+
+const createDefaultClient = (): Client => {
+  return new ApolloClient({
+    cache,
+    connectToDevTools: true,
+    defaultOptions: {
+      mutate: {
+        errorPolicy: 'all',
+      },
+      query: {
+        errorPolicy: 'all',
+      },
+      watchQuery: {
+        errorPolicy: 'all',
+      },
+    },
+    link: ApolloLink.from([httpLink, errorLink]),
+  });
+};
+
+/* eslint-disable-next-line import/no-mutable-exports */
+export let defaultClient = createDefaultClient();
+
+const createClient = async (): Promise<[Client, ClientDestroyer]> => {
+  defaultClient = createDefaultClient();
+  const destroyClient = await init(defaultClient);
+  return [defaultClient, destroyClient];
+};
+
+export default createClient;

f7e6a46092a014d4a8313c3175584720364af42e
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2022-04-17 14:55:48 -0300

    refactor(App.tsx): ignore ViewPropTypes warning
    The 'ViewPropTypes' is not being used directly in this application, it's coming
    from some lib. So, it could be safely ignored
    ref: https://github.com/facebook/react-native/issues/33557

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 
 import Navigation from 'navigation';
 
 import GraphQLContext from 'context/graphql';
+
+// The 'ViewPropTypes' is not being used directly in this application,
+// it's coming from some lib. So, it could be safely ignored
+// ref: https://github.com/facebook/react-native/issues/33557
+LogBox.ignoreLogs(['ViewPropTypes will be removed']);
 
 const App = (): JSX.Element => {
   return (

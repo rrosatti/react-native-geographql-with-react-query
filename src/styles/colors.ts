56b0aacc09b4d4aa2d271dbe557bcece34fc7372
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2022-04-17 14:56:17 -0300

    feat(styles/colors): define colors used in the project
    Define the colors that will be used in the project, to make it easier to export
    and use it.

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+const BaseColors = {
+  black: 'rgba(1, 1, 1, 1)',
+  white: 'rgba(255, 255, 255, 1)',
+};
+
+const CustomColors = {
+  graniteGray: 'rgba(102, 102, 110, 1)', // secondary text color on Country Tile
+  platinum: 'rgba(230, 230, 233, 1)', // Country Tile's background color
+};
+
+const Colors = {
+  ...BaseColors,
+  ...CustomColors,
+};
+
+export default Colors;

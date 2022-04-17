4544c51044695ff187daa7ebb48c94b65e0516ed
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2022-04-17 12:58:21 -0300

    fix(metro.config): configure metro to understand the '.cjs' file extension
    Fix necessary due to an error in the "@apollo/client" lib. This fix was
    suggested here: https://github.com/apollographql/apollo-client/blob/main/CHANGELOG.md#apollo-client-354-2021-11-19

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,16 @@
  * @format
  */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { getDefaultConfig } = require('metro-config');
+
+const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues();
+
 module.exports = {
+  resolver: {
+    ...defaultResolver,
+    sourceExts: [...defaultResolver.sourceExts, 'cjs'],
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {

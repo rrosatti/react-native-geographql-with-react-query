fa5abe981c00ca49764d747a052a9224ec8c849d
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2022-04-17 16:56:23 -0300

    feat(styles/colors): add new color
    Add color 'black coral'. It will be used in the 'InfoItem' component.

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -4,6 +4,7 @@ const BaseColors = {
 };
 
 const CustomColors = {
+  blackCoral: 'rgba(70, 83, 98, 1)', // InfoItem's value color
   graniteGray: 'rgba(102, 102, 110, 1)', // secondary text color on Country Tile
   platinum: 'rgba(230, 230, 233, 1)', // Country Tile's background color
 };

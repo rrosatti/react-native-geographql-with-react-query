b09bfd46d414ecfc8b76f88887969c22238a2905
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2022-04-17 14:57:21 -0300

    refactor(Countries): update route to query and display data from server
    Update the page to get data from the server through the 'useGetCountries()' hook
    and display them using the 'CountryList' component.

--- src/routes/Countries/index.tsx
+++ src/routes/Countries/index.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
+
+import useGetCountries, {
+  Constants as GetCountriesConstants,
+} from 'state/remote/use-get-countries';
+
+import LoadingOrError from 'components/LoadingOrError';
+import CountryList from 'components/CountryList';
 
 import styles from './styles';
 
 const Countries = (): JSX.Element => {
+  const { loading, error, data, refetch } = useGetCountries({
+    notifyOnNetworkStatusChange: true,
+    variables: {
+      pageCountries: { first: GetCountriesConstants.countries.defaultFirst },
+    },
+  });
+
+  const mappedCountries = React.useMemo(
+    () => data?.countries.edges.map((country) => ({ ...country.node })) || [],
+    [data],
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Countries</Text>
-    </View>
+    <LoadingOrError
+      loading={loading}
+      error={error}
+      data={data}
+      refetch={refetch}
+    >
+      <SafeAreaView style={styles.container}>
+        <View style={styles.contentContainer}>
+          <View style={styles.headerTextContainer}>
+            <Text style={styles.headerText}>Countries</Text>
+          </View>
+          <CountryList countries={mappedCountries} />
+        </View>
+      </SafeAreaView>
+    </LoadingOrError>
   );
 };
 
--- src/routes/Countries/styles.ts
+++ src/routes/Countries/styles.ts
@@ -1,10 +1,24 @@
 import { StyleSheet } from 'react-native';
 
+import Fonts from 'styles/fonts';
+
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  headerText: {
+    fontFamily: Fonts.NunitoSansBold,
+    fontSize: 24,
+  },
+  headerTextContainer: {
+    alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 8,
+    marginTop: 16,
   },
 });
 

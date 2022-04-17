76720ab0bd1974762e058f0af2409f61e4ab7df8
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2022-04-17 16:57:46 -0300

    feat(LoggedNavigator): use 'country navigator' standalone routes
    Use the 'standaloneRoutes' from 'CountryNavigator' and pass them to the Stack.

--- src/navigation/LoggedNavigator/index.tsx
+++ src/navigation/LoggedNavigator/index.tsx
@@ -5,10 +5,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 import routeNames from 'routes';
 
 import HomeTabNavigator from 'navigation/HomeNavigator';
+import { standaloneRoutes as countryStandaloneRoutes } from 'navigation/CountryNavigator';
 
 import type { NavigationParams } from './types';
 
 const Stack = createStackNavigator<NavigationParams>();
+
+const standaloneRoutes = {
+  ...countryStandaloneRoutes,
+};
 
 const LoggedNavigator = (): JSX.Element => (
   <Stack.Navigator
@@ -19,6 +24,13 @@ const LoggedNavigator = (): JSX.Element => (
       name={routeNames.HomeTabNavigator as keyof NavigationParams}
       component={HomeTabNavigator}
     />
+    {Object.entries(standaloneRoutes).map(([name, component]) => (
+      <Stack.Screen
+        name={name as keyof NavigationParams}
+        component={component}
+        key={name}
+      />
+    ))}
   </Stack.Navigator>
 );
 

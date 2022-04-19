f29e542dee13df349e79b3e778f7ceef9a2dca40
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2022-04-18 21:11:08 -0300

    refactor(state/create-client): update cache to use merge function
    The 'merge' function is going to be used to "combine" incoming data with
    "existing" cached data. At the moment, only the 'countries' field is going to
    need this to implement pagination / fetchMore.

--- src/state/create-client.ts
+++ src/state/create-client.ts
@@ -24,7 +24,28 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
-const cache = new InMemoryCache();
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        countries: {
+          keyArgs: [],
+          merge(existing, incoming) {
+            if (existing && existing.edges && incoming.edges) {
+              const merged = {
+                ...existing,
+                ...incoming,
+                edges: [...existing.edges, ...incoming.edges],
+              };
+              return merged;
+            }
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
 
 const init = async (
   client: ApolloClient<NormalizedCacheObject>,

c0e4e9ef806f25390e5703fd01f25f11270694f9
Rodrigo Rosatti Galvão <rosattig@profusion.mobi>
2022-04-20 20:38:29 -0300

    feat(styles/colors): add new colors
    Add two new colors that will be used in the "favorite"/"unfavorite" button.

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -5,6 +5,8 @@ const BaseColors = {
 
 const CustomColors = {
   blackCoral: 'rgba(70, 83, 98, 1)', // InfoItem's value color
+  dartmouthGreen: 'rgba(48, 107, 52, 1)', // "favorite button" background color
+  englishVermillion: 'rgba(214, 69, 80, 1)', // "unfavorite button" background color
   graniteGray: 'rgba(102, 102, 110, 1)', // secondary text color on Country Tile
   platinum: 'rgba(230, 230, 233, 1)', // Country Tile's background color
 };

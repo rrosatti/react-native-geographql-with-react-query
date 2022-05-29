430a6f4a4f3c52dff8dcc26d908a1b81d22eb939
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2022-05-29 14:33:13 -0300

    refactor(FavoriteUnfavoriteButto): update component to use styled-components
    Update component to use styled-components.

--- src/components/FavoriteUnfavoriteButton/index.tsx
+++ src/components/FavoriteUnfavoriteButton/index.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
-import styles from './styles';
-
-type Type = 'favorite' | 'unfavorite';
-
-interface Props {
-  onPress: () => void;
-  type: Type;
-}
+import { StyledContainerView, StyledButtonText } from './styled-components';
+import type { Props } from './types';
 
 const FavoriteUnfavoriteButton = ({ onPress, type }: Props): JSX.Element => {
   return (
-    <View
-      style={[
-        styles.container,
-        type === 'favorite' ? styles.favorite : styles.unfavorite,
-      ]}
-    >
+    <StyledContainerView type={type}>
       <TouchableOpacity onPress={onPress}>
-        <Text style={styles.buttonText}>
+        <StyledButtonText>
           {type === 'favorite' ? 'Favorite' : 'Unfavorite'}
-        </Text>
+        </StyledButtonText>
       </TouchableOpacity>
-    </View>
+    </StyledContainerView>
   );
 };
 

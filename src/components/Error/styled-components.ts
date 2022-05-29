60c809a6d6e4545f6aea607fb6ac1c4c8431b92b
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2022-05-29 14:24:27 -0300

    refactor(Error): update component to use styled-components
    Update component to use styled-components.

---
+++ src/components/Error/styled-components.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native';
+
+export const StyledContainerView = styled.View`
+  flex: 1;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-top: 16px;
+  padding-bottom: 16px;
+`;
+
+export const StyledErrorMessageView = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+`;
+
+export const StyledErrorText = styled.Text`
+  color: rgba(1, 1, 1, 1);
+`;
+
+export const StyledButtonView = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+`;

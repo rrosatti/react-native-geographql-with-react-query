3d102e06c8ee7cc18b402a299f70c8747f6e17ae
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2022-05-29 13:41:41 -0300

    refactor(CountryInfo): update component to use styled-components
    Update component to use styled-components.

---
+++ src/components/CountryInfo/styled-components.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const StyledEmojiContainerView = styled.View`
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 22px;
+`;
+
+export const StyledEmojiText = styled.Text`
+  font-size: 48px;
+`;
+
+export const StyledInfoItemContainerView = styled.View`
+  padding-top: 6px;
+  padding-bottom: 6px;
+`;
